c78f56bde065e5f4929fac44a6de90e01f3b79c0
Victor Cleber <13708226+victor-cleber@users.noreply.github.com>
2024-03-12 13:50:20 +1300

    Added 2 public subnets + IG

--- main.tf
+++ main.tf
@@ -19,3 +19,53 @@ resource "aws_vpc" "main" {
    Name = "VPC Project"
  }
 }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_subnet" "public_subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public1"
+  }
+}
+
+
+resource "aws_subnet" "public_subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public2"
+  }
+}
+
+resource "aws_internet_gateway" "ig_gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_ig"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig_gw.id
+  }
+
+  tags = {
+    Name = "public_route_table"
+  }
+}
+
