0f9dcb1929af3f13c75a7470fd603b1f09ed69c3
Victor Cleber <13708226+victor-cleber@users.noreply.github.com>
2024-03-12 13:43:56 +1300

    Added vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {    
+    aws= {
+      source ="hashicorp/aws"
+      version = "~>4.0.0"
+    }
+  }
+}
 provider "aws" {
-  region = "eu-west-1"
+    region = "ap-southeast-2"
+    profile= "default"
+}
+
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ enable_dns_hostnames =  true
+ enable_dns_support = true
+ tags = {
+   Name = "VPC Project"
+ }
 }
