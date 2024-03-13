ccb7e3f7e7158caf808ad1db4b7a15e4b8b16bb1
Victor Cleber <13708226+victor-cleber@users.noreply.github.com>
2024-03-13 17:43:33 +1300

    Defining AZs

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2a"
 
  tags = {
     Name = "public_subnet_1"
@@ -34,6 +35,7 @@ resource "aws_subnet" "public_subnet1" {
 resource "aws_subnet" "public_subnet2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-southeast-2b"
 
  tags = {
     Name = "public_subnet_2"
