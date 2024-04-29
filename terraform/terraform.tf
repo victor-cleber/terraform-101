23f62dcf7d8e623c08b1b0dcb029a848fbe32340
Victor Ferreira da Silva <ferreiradasv@vector.co.nz>
2024-04-29 23:19:18 +1200

    Formated

---
+++ terraform/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {    
+    aws= {
+      source ="hashicorp/aws"
+      #version = "~>4.0.0"
+    }
+  }
+}
+provider "aws" {
+  region = "ap-southeast-2"
+  #profile= "default"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "vc-workshop-17"
+    key    = "infra"
+    region = "ap-southeast-2"
+    encrypt        = true
+  }
+}
